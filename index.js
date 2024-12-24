0a6998bb85978af44157f7a79d02a47dc30fed0d
motiur-rahman-dhrubo <mdmotiurrahman383@gmail.com>
2024-12-24 20:26:57 +0600

    car uploading to detabase successfully

--- index.js
+++ index.js
@@ -35,10 +35,16 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
+    const carCollection = client.db('carDB').collection('car');
+
+    app.post('/car', async(req, res)=> {
+        const newCar = req.body;
+        console.log(newCar);
+        const result = await carCollection.insertOne(newCar);
+        res.send(result);
+    })
 
 
-
-    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
