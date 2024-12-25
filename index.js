4c24263bb567f3cb3bef08c17e2a4cb83cbf1f8b
motiur-rahman-dhrubo <mdmotiurrahman383@gmail.com>
2024-12-26 02:37:32 +0600

    using jwt successfully

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
@@ -11,23 +11,31 @@ const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: ["http://localhost:5173"],
-    credentials: true
+    credentials: true,
   })
 );
 app.use(express.json());
 app.use(cookieParser());
 
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
 
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
 
+  // verify the token
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
 
-
-
-
-
-
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xrrul.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -43,34 +51,38 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const carCollection = client.db('carDB').collection('car');
-
+    const carCollection = client.db("carDB").collection("car");
 
     // auth related APIs
-    app.post('/jwt', (req, res) =>{
+    app.post("/jwt", (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s' });
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "6h",
+      });
 
-      res.cookie('token', token, {
-        httpOnly: true,
-        secure: false
-      })
-      .send({ success: true })
-    })
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
 
-    app.post('/logout', (req, res) => {
-      res.clearCookie('token', {
-        httpOnly: true,
-        secure: false
-      })
-      .send({ success: true })
-    })
+    app.post("/logout", (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
 
-    app.get("/my_car", async (req, res) => {
+    app.get("/my_car", verifyToken, async (req, res) => {
       const email = req.query.email;
-      let query = {};
-      if (email) {
-        query = { user_email: email };
+      let query = { user_email: email };
+
+      if (req.user.email !== req.query.email) {
+        return req.status(403).send({ message: "forbidden access" });
       }
 
       const cursor = carCollection.find(query);
@@ -78,12 +90,12 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/available_car', async(req, res)=> {
-        const query = { availability: "Available" };
-        const cursor = carCollection.find(query);
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    app.get("/available_car", async (req, res) => {
+      const query = { availability: "Available" };
+      const cursor = carCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     app.get("/car/:id", async (req, res) => {
       const id = req.params.id;
@@ -92,13 +104,12 @@ async function run() {
       res.send(result);
     });
 
-    app.post('/car', async(req, res)=> {
-        const newCar = req.body;
-        console.log(newCar);
-        const result = await carCollection.insertOne(newCar);
-        res.send(result);
-    })
-
+    app.post("/car", async (req, res) => {
+      const newCar = req.body;
+      console.log(newCar);
+      const result = await carCollection.insertOne(newCar);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -112,12 +123,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-    res.send('jo.car server is running')
+app.get("/", (req, res) => {
+  res.send("jo.car server is running");
 });
 
 app.listen(port, () => {
-    console.log(`jo.car server is running on port: ${port}`)
-})
+  console.log(`jo.car server is running on port: ${port}`);
+});
